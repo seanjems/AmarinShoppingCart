59ec8f32656535cc95614a6e65d7967f19ec7890
Najuna James <seanjems@gmail.com>
2022-03-08 15:06:12 +0300

    Added and completed API CRUD methods for Pages. Added seed data for Initializing Pages

--- AmarinShoppingCart/Controllers/PagesController.cs
+++ AmarinShoppingCart/Controllers/PagesController.cs
@@ -19,9 +19,58 @@ namespace AmarinShoppingCart.Controllers
             this.context = context;
         }
 
+        //GET /api/pages
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<Page>>> GetPages()
         {
             return await context.pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+
+        //GET /api/pages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Page>> GetPage (int id)
+        {
+            var page = await context.pages.FindAsync(id);
+            if (page==null)
+            {
+                return NotFound();
+            }
+            return page;
+        }
+
+        //PUT /api/pages/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Page>> PutPage(int id, Page page)
+        {
+            if (page.Id != id)
+            {
+                return BadRequest();
+            }
+            context.Entry(page).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        //POST /api/pages
+        [HttpPost]
+        public async Task<ActionResult<Page>> PostPage(Page page)
+        {
+
+            context.pages.Add(page);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostPage), page);
+        }
+
+        //DELETE /api/pages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Page>> DeletePage(int id)
+        {
+            var page = context.pages.Find(id);
+            context.pages.Remove(page);
+            await context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
---
+++ AmarinShoppingCart/Models/SeedData.cs
@@ -0,0 +1,46 @@
+﻿
+using AmarinShoppingCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmarinShoppingCart.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AmarinShoppingCartContext
+                (serviceProvider.GetRequiredService<DbContextOptions<AmarinShoppingCartContext>>()))
+            {
+                if (context.pages.Any())
+                {
+                    return;
+                }
+
+                context.pages.AddRange(
+                    new Page
+                    {
+                        Title = "Home",
+                        Slug = "home",
+                        Content = "This is the default home page Please customise it. All static files for the home page can be injected here via CK editor.",
+                        Sorting = 0
+                    },
+                     new Page
+                     {
+                         Title = "About us",
+                         Slug = "about-us",
+                         Content = "This page holds all the static files related to about us",
+                         Sorting = 100
+                     }
+                    
+
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
