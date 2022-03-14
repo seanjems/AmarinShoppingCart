a67beae03478ecf1e4940e42eded04abac99e1fb
Najuna James <seanjems@gmail.com>
2022-03-14 09:36:29 +0300

    Connected the Home page to the Backend

--- ReactJSFrontEnd/AmarinCartUI/src/components/Home.jsx
+++ ReactJSFrontEnd/AmarinCartUI/src/components/Home.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
-class Home extends Component {
-  state = {};
+class Categories extends Component {
+  //declare an array to store data from server
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Page: [],
+    };
+  }
+
+  //method to get api data
+  refreshList() {
+    //let isMounted = true; // to prevent Setting DOMdata during unmounting phase
+    fetch(process.env.REACT_APP_API_Home)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ Page: data });
+      });
+  }
+
+  //update Dom on initialization
+  componentDidMount() {
+    this.refreshList();
+  }
+
   render() {
+    const rawHTML = this.state.Page.content;
+
     return (
-      <div className="mt-5 d-flex justify-content-left">
-        This is the home page.
+      <div>
+        <br />
+        {<div dangerouslySetInnerHTML={{ __html: rawHTML }} />}
       </div>
     );
   }
 }
 
-export default Home;
+export default Categories;
